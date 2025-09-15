dd383aec88da29ea61ab406df702733ae752d529
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2025-09-15 14:07:32 -0300

    Refactor la función de comparación para consolidar la lógica y mejorar la legibilidad

--- day1-comparacionVariables.js
+++ day1-comparacionVariables.js
@@ -6,20 +6,16 @@ let stringTreinta = '30'
 let numeroDiez = 10
 let stringDiez = '10'
 
-if (numeroUn == stringUn) {
-  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
-} else {
-  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+function compararTiposYValores(var1, var2, nombre1, nombre2) {
+  if (var1 === var2) {
+    console.log(`Las variables ${nombre1} y ${nombre2} tienen el mismo valor y el mismo tipo`);
+  } else if (var1 == var2) {
+    console.log(`Las variables ${nombre1} y ${nombre2} tienen el mismo valor, pero tipos diferentes`);
+  } else {
+    console.log(`Las variables ${nombre1} y ${nombre2} no tienen el mismo valor`);
+  }
 }
 
-if (numeroTreinta == stringTreinta) {
-  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
-} else {
-  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
-}
-
-if (numeroDiez == stringDiez) {
-  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
-} else {
-  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
-}
+compararTiposYValores(numeroUn, stringUn, 'numeroUn', 'stringUn');
+compararTiposYValores(numeroTreinta, stringTreinta, 'numeroTreinta', 'stringTreinta');
+compararTiposYValores(numeroDiez, stringDiez, 'numeroDiez', 'stringDiez');
